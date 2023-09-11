394aa1aab7b52be39e1ca7bca220f200d2eb8491
arizalsetiawan <44498602+arizalsetiawan@users.noreply.github.com>
2023-09-11 22:32:01 +0700

    Add files via upload

---
+++ readfile.js
@@ -0,0 +1,37 @@
+/*
+  * DannTeam
+  * IzumiTeam
+*/
+
+// Big Thanks To
+// - All Creator
+// - All Supporter
+// - All Donationer
+// - All Progammer
+// - All Team
+// - All Community
+// - All People
+// - God [ ! ]
+// - Family [ ! ]
+
+// Special Thanks To
+// - [ Izumi Nazora] > owner Botz
+// - [ DannLonely ] > Pengembang dari Dann-MD
+// - [ NANS OFFICIAL ] > Promoter
+// - [ VynaChan ] > Promoter
+// - [ DannTeam ] > Pengembang semua fitur Dann-MD
+// - [ Arippanteq ] > Helper
+// - [ Skyline ] > Mengembangkan apikey dengan unlimited
+// - [ Kaito Saikyo ] > Moderators / Teams
+// - [ Dann ] > Administrator
+
+// Jangan dihapus.
+// Berterimakasihlah jika masih ada script seperti begini
+// All Team menyanyangi kamu jika kamu menghargai script yang di buat oleh * DannLonely *
+// Kami sangat berterimakasih untuk kalian yang sudah mendukung dari Donasi, dan lain-lain.
+
+// Logger for thanks you
+console.log('Thanks To Inetplus')
+
+// Jum'at, 12/05/23
+// 00.00 WITA
